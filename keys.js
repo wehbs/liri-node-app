33844edd211585aa80e37dac51954a13e654e557
wehbs <shamirwehbe@me.com>
2017-10-09 15:25:56 -0400

    setup folder structure

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'qEjwrYvTSISKnSlcpRrogWeoP',
+  consumer_secret: 'T6S9eV959XEOjN5DUePNGkUMZYwozKF99l3bM1ytMWJxfJjUdI',
+  access_token_key: '917458135622840320-HMDyvOgtCTfzxksAwa11KuzoZtyFiZB',
+  access_token_secret: 'XzO7crxSddB04qKI7FB0CCcCvhY8P5U1TfYn0joZwOnzp',
+}
+
+module.exports = twitterKeys;
