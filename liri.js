fa6fb17bbf383a4fa8c69f929c05598688090546
wehbs <shamirwehbe@me.com>
2017-10-09 22:30:52 -0400

    default search for mr nobody if no movie typed in added

--- liri.js
+++ liri.js
@@ -48,22 +48,43 @@ function spotify() {
 
 function imdb() {
 
-    var movieQuery = 'http://www.omdbapi.com/?apikey=40e9cece&t=' + value;
 
-    var request = require('request');
-    request(movieQuery, function (error, response) {
-        var results = JSON.parse(response.body);
+    if (value === undefined) {
 
-        console.log('Movie Title: ' + results.Title);
-        console.log('Release Year: ' + results.Year);
-        console.log(results.Ratings[0].Source + ' ' + results.Ratings[0].Value);
-        console.log(results.Ratings[1].Source + ' ' + results.Ratings[1].Value);        
-        console.log('Country: ' + results.Country);
-        console.log('Language: ' + results.Language);        
-        console.log('Plot: ' + results.Plot);
-        console.log('Actors: ' + results.Actors);
-        
-    });
+        defaultSearch = 'http://www.omdbapi.com/?apikey=40e9cece&t=mr.nobody'
+        var request = require('request');
+        request(defaultSearch, function (error, response) {
+            var results = JSON.parse(response.body);
+
+            console.log('Movie Title: ' + results.Title);
+            console.log('Release Year: ' + results.Year);
+            console.log(results.Ratings[0].Source + ' ' + results.Ratings[0].Value);
+            console.log(results.Ratings[1].Source + ' ' + results.Ratings[1].Value);
+            console.log('Country: ' + results.Country);
+            console.log('Language: ' + results.Language);
+            console.log('Plot: ' + results.Plot);
+            console.log('Actors: ' + results.Actors);
+
+        });
+
+    } else {
+        var movieQuery = 'http://www.omdbapi.com/?apikey=40e9cece&t=' + value;
+
+        var request = require('request');
+        request(movieQuery, function (error, response) {
+            var results = JSON.parse(response.body);
+
+            console.log('Movie Title: ' + results.Title);
+            console.log('Release Year: ' + results.Year);
+            console.log(results.Ratings[0].Source + ' ' + results.Ratings[0].Value);
+            console.log(results.Ratings[1].Source + ' ' + results.Ratings[1].Value);
+            console.log('Country: ' + results.Country);
+            console.log('Language: ' + results.Language);
+            console.log('Plot: ' + results.Plot);
+            console.log('Actors: ' + results.Actors);
+
+        });
+    }
 }
 
 function doIt() {
